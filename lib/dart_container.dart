6d45e6d7d1398efd728f08f45a7b57d54f44b472
ctrohin <cristi.trohin@gmail.com>
2024-06-20 10:11:02 +0300

    Export everything needed

--- lib/dart_container.dart
+++ lib/dart_container.dart
@@ -4,6 +4,12 @@
 library;
 
 export 'src/dart_container_base.dart'
-    show injectorRegister, injectorGet, injectorGetIfPresent, Container;
+    show
+        injectorRegister,
+        injectorGet,
+        injectorGetIfPresent,
+        injectorRegisterLazy,
+        injectorRegisterFactory,
+        Container;
 
 // TODO: Export any libraries intended for clients of this package.
